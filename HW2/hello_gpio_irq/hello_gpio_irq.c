b667c926fec9817284537737d1d56dd3b84e569e
viollbailey <violetbailey2022@u.northwestern.edu>
2025-04-07 18:21:05 -0500

    Yay, I smushed everything together and finished it!
    These probably should've been separate commits

--- HW2/hello_gpio_irq/hello_gpio_irq.c
+++ HW2/hello_gpio_irq/hello_gpio_irq.c
@@ -8,25 +8,63 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
-#define GPIO_WATCH_PIN 25
+#define GPIO_WATCH_PIN 20
 
-static char event_str[128];
+#ifdef CYW43_WL_GPIO_LED_PIN // For the LED part of the code
+#include "pico/cyw43_arch.h"
+#endif
 
-void gpio_event_string(char *buf, uint32_t events);
+#ifndef LED_DELAY_MS // For the LED part of the code
+#define LED_DELAY_MS 250
+#endif
+
+static int count = 0;
+
+static int ledFlag = 1; // controlling whether the light is on or not (will switch to On at first button press)
+
+// Perform initialisation
+int pico_led_init(void) {
+#if defined(PICO_DEFAULT_LED_PIN)
+    // A device like Pico that uses a GPIO for the LED will define PICO_DEFAULT_LED_PIN
+    // so we can use normal GPIO functionality to turn the led on and off
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    return PICO_OK;
+#elif defined(CYW43_WL_GPIO_LED_PIN)
+    // For Pico W devices we need to initialise the driver etc
+    return cyw43_arch_init();
+#endif
+}
+
+// Turn the led on or off
+void pico_set_led(bool led_on) {
+    #if defined(PICO_DEFAULT_LED_PIN)
+        // Just set the GPIO on or off
+        gpio_put(PICO_DEFAULT_LED_PIN, led_on);
+    #elif defined(CYW43_WL_GPIO_LED_PIN)
+        // Ask the wifi "driver" to set the GPIO on or off
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
+    #endif
+    }
 
 void gpio_callback(uint gpio, uint32_t events) {
-    // Put the GPIO event(s) that just happened into event_str
-    // so we can print it
-    gpio_event_string(event_str, events);
-    printf("GPIO %d %s\n", gpio, event_str);
+    pico_set_led(ledFlag);
+    ledFlag = !ledFlag;
+    sleep_ms(LED_DELAY_MS);
+    count++;
+    printf("Button pressed %d times since last reset \r\n", count);
 }
+
 
 int main() {
     stdio_init_all();
-    
+
+    int rc = pico_led_init();
+    hard_assert(rc == PICO_OK);
+
     printf("Hello GPIO IRQ\n");
     gpio_init(GPIO_WATCH_PIN);
-    gpio_set_irq_enabled_with_callback(GPIO_WATCH_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+    gpio_set_irq_enabled_with_callback(GPIO_WATCH_PIN, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
 
     // Wait forever
     while (1);
@@ -39,24 +77,3 @@ static const char *gpio_irq_str[] = {
         "EDGE_FALL",  // 0x4
         "EDGE_RISE"   // 0x8
 };
-
-void gpio_event_string(char *buf, uint32_t events) {
-    for (uint i = 0; i < 4; i++) {
-        uint mask = (1 << i);
-        if (events & mask) {
-            // Copy this event string into the user string
-            const char *event_str = gpio_irq_str[i];
-            while (*event_str != '\0') {
-                *buf++ = *event_str++;
-            }
-            events &= ~mask;
-
-            // If more events add ", "
-            if (events) {
-                *buf++ = ',';
-                *buf++ = ' ';
-            }
-        }
-    }
-    *buf++ = '\0';
-}
