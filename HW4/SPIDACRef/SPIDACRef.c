1caf0f2b2fe5528042ed60d632540ee1cec0183b
viollbailey <violetbailey2022@u.northwestern.edu>
2025-04-15 06:57:32 -0500

    testing out spi channel

--- HW4/SPIDACRef/SPIDACRef.c
+++ HW4/SPIDACRef/SPIDACRef.c
@@ -2,7 +2,21 @@
 #include "pico/stdlib.h"
 #include "pico/binary_info.h" // not included
 #include "hardware/spi.h"
+#include "math.h"
 
+void writeDAC(int, float); // channel, voltage
+
+static inline void cs_select(uint cs_pin) { // inline forces the program to copy and paste the function, speeding up at cost of memory
+    asm volatile("nop \n nop \n nop"); // Assembly code to skip clock cycles, keeps us from flipping the pin too quickly
+    gpio_put(cs_pin, 0);
+    asm volatile("nop \n nop \n nop"); // 
+}
+
+static inline void cs_deselect(uint cs_pin) {
+    asm volatile("nop \n nop \n nop"); // 
+    gpio_put(cs_pin, 1);
+    asm volatile("nop \n nop \n nop"); // 
+}
 
 int main()
 {
@@ -16,6 +30,8 @@ int main()
    
     // Initializing the SPI
     spi_init(spi_default, 12000); // the baud, or bits per second. Want it to be slow for debugging
+                                    // 12kHz = slow
+                                    // Maximum is bandwidth of nScope (100kHz)
     gpio_set_function(PICO_DEFAULT_SPI_RX_PIN, GPIO_FUNC_SPI);
     gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
     gpio_set_function(PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SPI);
@@ -25,13 +41,44 @@ int main()
     gpio_init(PICO_DEFAULT_SPI_CSN_PIN);
     gpio_put(PICO_DEFAULT_SPI_CSN_PIN, 1);
     gpio_set_dir(PICO_DEFAULT_SPI_CSN_PIN, GPIO_OUT);
-
-    /* SPI data written with the following
-    
-    cs_select(PICO_DEFAULT_SPI_CSN_PIN); //Makes the chip select pin go low, activating it
-    spi_write_blocking(SPI_PORT, data, len); // where data is a uint8_t array with length len
-    cs_deselect(PICO_DEFAULT_SPI_CSN_PIN); // Makes the chip select pin go high, disabling it
-    
-    */
    
+    float t = 0; // Time tracker
+    float v = 0; // Voltage tracker
+
+    while(1){
+        printf("I'm doing stuff");
+        sleep_ms(500);
+        
+        /* Main block for writing
+        for(int i = 0; i < 100; i++){
+            sleep_ms(10); // Update rate needs to be 50 times faster than freq, so 100 Hz should work (10ms update time)
+            t = t + 0.1;
+            v = 3.3 * (sin(t) + 1) / 1024;
+            writeDAC(0,v);
+        }
+        */
+       writeDAC(0,0); // Hard code testing 
+    }
+
+    return 0;
+#endif
+}
+
+
+void writeDAC(int channel, float voltage){
+    uint8_t data[2];
+    int len = 2;
+
+    /*
+    data[0] = 0;
+    data[0] = data[0] | (channel<<7);
+    data[0] = data[0] | (0b111<<4);
+    data[0] = data[0] | ();
+    data[1] = 255;
+    */
+    data[0] = 0b01010101;
+    data[1] = 255;
+    cs_select(PICO_DEFAULT_SPI_CSN_PIN); //Makes the chip select pin go low, activating it
+    spi_write_blocking(spi_default, data, len); // where data is a uint8_t array with length len
+    cs_deselect(PICO_DEFAULT_SPI_CSN_PIN); // Makes the chip select pin go high, disabling it
 }
